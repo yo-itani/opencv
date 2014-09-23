3349d3f6738774bff25870d7526c6dd16582c7db
Your Name <you@example.com>
2014-09-23 12:30:31 +0900

    bing search apiから画像を取得するプログラムを追加

---
+++ develop/get_image_file.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+import bing_api.bing_api as bing_api
+
+import os
+import time
+
+import cv2
+import requests
+
+import color_utils as cu
+from secrets import BING_API_KEY
+
+TOP = 50
+IMG_INFO_TSV = 'img_info.tsv'
+IMG_INFO_TSV_COLUMNS = ('No.', 'image url', 'image file', 'source url',)
+CREATE_TSV_CMD = 'echo "%s" >> %%s' % ('\t'.join(['%s'] * len(IMG_INFO_TSV_COLUMNS)),)
+WIDTH = 200
+
+
+def main(args):
+    query = args.query
+    num = args.num
+    dest_dir = args.dest_dir
+    counter = 0
+    data_tsv = os.path.join(dest_dir, 'img_info.tsv')
+    if os.path.isfile(data_tsv):
+        os.remove(data_tsv)
+    os.system(CREATE_TSV_CMD % (IMG_INFO_TSV_COLUMNS + (data_tsv,)))
+    for skip in xrange(0, num, TOP):
+        for item in bing_api.get(BING_API_KEY, query, bing_api.IMAGE, bing_api.JSON, TOP, skip)['d']['results']:
+            content_type = item.get('ContentType', '')
+            image_url = item.get('MediaUrl', '')
+            source_url = item.get('SourceUrl', '')
+            if content_type and image_url:
+                img_file = get_img(image_url, content_type, counter, dest_dir)
+                if img_file:
+                    resize_img(img_file, dest_dir, WIDTH)
+                    os.system(CREATE_TSV_CMD % (counter, image_url, img_file, source_url, data_tsv))
+                    counter += 1
+        time.sleep(1)
+        print '%s〜%s done' % (skip, skip + TOP,)
+
+
+def resize_img(img_file, dest_dir, width):
+    """ 画像ファイルを引数で与えられた幅に縮める
+
+        Args:
+            img_file: 画像ファイル名
+            dest_dir: 画像ファイルがあるディレクトリ
+            width: 縮小(拡大)後の画像ファイルのサイズ(幅)
+        Returns:
+            なし
+    """
+    fpath_file = os.path.join(dest_dir, img_file)
+    img = cv2.imread(fpath_file)
+    resized_img = cu.get_resized_img(img, width)
+    cv2.imwrite(fpath_file, resized_img)
+
+
+def get_img(image_url, content_type, number, dest_dir):
+    """ 画像をダウンロードしファイルとして保存する
+
+        Args:
+            image_url: イメージファイルのURL
+            content_type: 服のタイプ
+            number: イメージファイルにつける連番
+            dest_dir: イメージファイル保存先
+        Returns:
+            取得成功: 作成した画像ファイル名  取得失敗: None
+    """
+    img_type = dest_dir.split('/')[-1]
+    ext = get_ext(content_type)
+    if not ext:
+         ext = os.path.splitext(image_url)
+    img_file = '%s.%08d%s' % (img_type, number, ext,)
+    r = requests.get(image_url)
+    if r.headers['content-type'].startswith(content_type) and \
+       r.status_code == 200:
+        fp = open(os.path.join(dest_dir, img_file), 'wb')
+        fp.write(r.content)
+        fp.close()
+        return img_file
+    return None
+
+
+def get_ext(content_type):
+    """ コンテントタイプから拡張子を決定して返す。
+        決定できないcontent typeの場合はNoneをかえす。
+
+        Args:
+            content_type: コンテントタイプ
+        Returns:
+            コンテントタイプから判断した拡張子(str)。
+            判断できなかったときはNone
+    """
+    content_type = content_type.lower()
+    if content_type == 'image/gif':
+        return '.gif'
+    elif content_type in ['image/jpeg', 'image/jpg',]:
+        return '.jpg'
+    elif content_type in ['image/x-png', 'image/png',]:
+        return '.png'
+    elif content_type in ['image/tiff', 'image/x-tiff',]:
+        return '.tif'
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--query', '-q', action='store', required=True)
+    parser.add_argument('--num', '-n', action='store', type=int, required=True)
+    parser.add_argument('--dest_dir', '-d', action='store', required=True)
+    args = parser.parse_args()
+    main(args)
